23ad32ab1c2f2368f6dd39839f299a10887ce962
Dhanush Urs M K <dhanushursmk@Dhanushs-MacBook-Air.local>
2025-08-27 16:07:53 +0530

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
-app.get('/', (req, res) => res.send('Hello from DevOps Project 🚀'));
+app.get('/', (req, res) => res.send('hello mam and sir'));
 
 if (require.main === module) {
   app.listen(port, () => console.log(`Listening on ${port}`));

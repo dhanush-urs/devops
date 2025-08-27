92384366c3486708f42924fcfb446262adcfc52d
Dhanush Urs M K <dhanushursmk@Dhanushs-MacBook-Air.local>
2025-08-27 16:24:55 +0530

    again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
-app.get('/', (req, res) => res.send('hello madam'));
+app.get('/', (req, res) => res.send('hello madam and sirs'));
 
 if (require.main === module) {
   app.listen(port, () => console.log(`Listening on ${port}`));
